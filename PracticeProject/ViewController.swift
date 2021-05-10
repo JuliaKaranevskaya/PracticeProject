a8921756232394c0d0d517f2ffcd01a9a24e21f3
Юлия <julia.karanevskaya@gmail.com>
2021-05-10 21:09:54 +0300

    Initial Commit

---
+++ PracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeProject
+//
+//  Created by Юлия Караневская on 10.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
